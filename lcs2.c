4ec6095d57a4c1690ff67a3710c77bd48292c87f
AJ-AX <asathyagal18@simons-rock.edu>
2019-03-18 03:49:35 -0400

    Finished and Tested

--- lcs2.c
+++ lcs2.c
@@ -3,14 +3,42 @@
 
 void print_array(int *arr, int size){
 	for (int i=0; i<size; i++){
-		printf("%d ",arr[i]);	
+		printf("[%d] ",arr[i]);	
 	}
 	printf("\n");
 }
 
+int max(int a, int b){
+	if(a > b){
+		return a;
+	}
+	else{
+		return b;
+	}
+}
+
 int lcs2(int *a, int n,  int *b, int m) {
 	//write your code here
-	return 0;
+	int data[n+1][m+1];
+
+	for(int i = 0; i < n+1; i++){
+		data[i][0] = 0;
+	}
+	for(int j = 0; j < m+1; j++){
+		data[0][j] = 0;
+	}
+	
+	for(int i = 1; i <= n; i++){
+		for(int j = 1; j <= m; j++){
+			if(a[i-1] == b[j-1]){
+				data[i][j] = data[i-1][j-1] + 1;
+			}
+			else{
+				data[i][j] = max(data[i-1][j], data[i][j-1]);
+			}
+		}
+	}
+	return data[n][m];
 }
 
 int main() {
--- lcs3.c
+++ lcs3.c
@@ -8,9 +8,53 @@ void print_array(int *arr, int size){
 	printf("\n");
 }
 
+int max(int a, int b){
+	if(a>b){
+		return a;
+	}
+	else{
+		return b;
+	}
+}
+
 int lcs3(int *a, int n,  int *b, int m, int *c, int l) {
 	//write your code here
-	return 0;
+	int data[n+1][m+1][l+1];
+
+	/*for(int i = 0; i < n+1; i++){
+		data[i][0][0] = 0;
+	}
+	for(int i = 0; i < m+1; i++){
+		data[0][i][0] = 0;
+	}
+	for(int i = 0; i < l+1; i++){
+		data[0][0][i] = 0;
+	}*/
+
+	for(int i = 0; i <= n; i++){
+		for(int j = 0; j <= m; j++){
+			for(int y = 0; y <= l; y++){
+				// Since we have to initialize 0s in 3 dimensions rather than 2
+				// We just do it inside the function
+				if(i == 0){
+					data[i][j][y] = 0;
+				}
+				else if(j == 0){
+					data[i][j][y] = 0;
+				}
+				else if(y == 0){
+					data[i][j][y] = 0;
+				}
+				else if(a[i-1] == b[j-1] && a[i-1] == c[y-1]){
+					data[i][j][y] = data[i-1][j-1][y-1] + 1;
+				}
+				else{
+					data[i][j][y] = max(max(data[i-1][j][y],data[i][j-1][y]),data[i][j][y-1]);
+				}
+			}
+		}
+	}
+	return data[n][m][l];
 }
 
 int main() {
